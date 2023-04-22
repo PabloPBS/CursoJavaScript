5405a05035f37229bfeb7e664434f0cc1e5f3b14
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-04-22 18:57:56 -0300

    Estruturas de repetição

---
+++ Exercícios/aula13/ambiente.js
@@ -0,0 +1,32 @@
+var c = 1
+do {
+    console.log('Hello World')
+    c++
+} while (c <= 6)
+
+
+/*var c = 1
+while (c <=  6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+
+for (var c = 0; c < 10; c++) {
+    console.log(c)
+}*/
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
