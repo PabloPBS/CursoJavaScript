b3541012052cff8bd2988c66766a5551ae0f0d9a
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-04-22 22:34:24 -0300

    ex017 criado

--- Exercícios/aula14ex/ex017/script.js
+++ Exercícios/aula14ex/ex017/script.js
@@ -1,2 +1,19 @@
-agora = new Date()
-hora = agora.getHours()
+function tabuada() {
+    let txtnum = document.getElementById('txtnum')
+    let tab = document.getElementById('seltab')
+
+    if (txtnum.value.length == 0) {
+        window.alert('Por favor, digite um número.')
+    } else {
+        let num = Number(txtnum.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
